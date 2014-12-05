ae151738b7828aec4e57e134368d5e094b5bd185
alumno215ecb16 <edher913@gmail.com>
2014-12-05 09:16:03 -0600

    primera subida

---
+++ jquery-mobile/acciones.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		
+		$('#1').on ("swipeleft", function(){
+			<a href="#Pagina13"></a>;
+			});//barrer izquierda
+			
+			
+			$('#derecha').on ("swiperight", function(){
+			<a href="Pagina14"></a>
+				
+			});//barrer derecha
+			
+			
+	},false);//ready device
+});//document
